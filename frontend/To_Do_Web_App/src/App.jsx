cf0d2374fcec355fdc335fd624158b4a447550a2
deelaka99 <123wejith@gmail.com>
2023-12-27 00:43:16 +0530

    initialize the project

---
+++ frontend/To_Do_Web_App/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div className="text-green">Deelaka</div>;
+}
+
+export default App;
---
+++ frontend/To_Do_Web_App/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      'green':'#00DD00',
+      'darkGreen':'#01532C',
+      'white': '#ffffff',
+      'black':'#000000',
+      'lightGrey':'#D9D9D9',
+      'lightGrey1':'#BBBBBB',
+      'lightGrey2':'#525252',
+      'grey':'#333333',
+      'darkGrey':'#282828',
+      'blue':'#6EB6FF',
+      'red' :'#FF0000',
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      jomhuria: ["Jomhuria", "cursive"]
+    },
+    screens:{
+      'mobile':'375px',
+      'tablet':'640px',
+      'laptop':'1024px',
+      'desktop':'1280px',
+    },
+  },
+  plugins: [],
+}
+
