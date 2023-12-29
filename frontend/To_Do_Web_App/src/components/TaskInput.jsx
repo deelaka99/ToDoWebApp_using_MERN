99553b95f58c1d38fc6fd9a91685b94667239bc6
deelaka99 <123wejith@gmail.com>
2023-12-30 00:14:24 +0530

    TaskInput is ready for tablets

--- frontend/To_Do_Web_App/src/components/TaskInput.jsx
+++ frontend/To_Do_Web_App/src/components/TaskInput.jsx
@@ -8,11 +8,11 @@ function TaskInput() {
       <div className="flex pt-3 gap-1">
         <input
           type="text"
-          className="bg-white font-inter mobile:w-[227px] mobile:h-[36px] mobile:rounded-[3px] mobile:pl-2 mobile:text-[12px] text-textGrey outline-none focus:ring-2 drop-shadow placeholder:text-placeholderGrey"
+          className="bg-white font-inter mobile:w-[227px] mobile:h-[36px] tablet:w-[400px] tablet:h-[55px] mobile:rounded-[3px] tablet:rounded-[5px] mobile:pl-2 tablet:pl-10 mobile:text-[12px] tablet:text-[20px] text-textGrey outline-none focus:ring-2 drop-shadow placeholder:text-placeholderGrey"
           placeholder="Enter your task here..."
         />
-        <button className="bg-green text-white h-auto w-auto mobile:px-2 mobile:rounded-[4px] drop-shadow active:bg-darkGrey active:drop-shadow-md">
-          <FontAwesomeIcon className="mobile:text-[16px]" icon={faPlus} />
+        <button className="bg-green text-white h-auto w-auto mobile:px-2 tablet:px-3 mobile:rounded-[6px] drop-shadow active:bg-darkGrey active:drop-shadow-md">
+          <FontAwesomeIcon className="mobile:text-[16px] tablet:text-[48px]" icon={faPlus} />
         </button>
       </div>
     </div>
