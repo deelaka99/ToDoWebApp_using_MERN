64bb4a98d32784ede2d555a24b3ecb0eda755e2e
deelaka99 <123wejith@gmail.com>
2023-12-30 01:13:18 +0530

    title is ready for laptops

--- frontend/To_Do_Web_App/src/components/Title.jsx
+++ frontend/To_Do_Web_App/src/components/Title.jsx
@@ -3,8 +3,8 @@ import React from "react"
 function Title() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-auto gap-1">
-      <p className="font-inter font-medium mobile:text-[28px] tablet:text-[55px] text-darkGrey">Simple | To Do App</p>
-      <p className="text-thirdGrey font-jomhuria mobile:text-[12px] tablet:text-[23px]">A basic to do app created using MERN stack</p>
+      <p className="font-inter font-medium mobile:text-[28px] tablet:text-[55px] laptop:text-[45px] text-darkGrey">Simple | To Do App</p>
+      <p className="text-thirdGrey font-jomhuria mobile:text-[12px] tablet:text-[23px] laptop:text-[19.5px]">A basic to do app created using MERN stack</p>
     </div>
   )
 }
