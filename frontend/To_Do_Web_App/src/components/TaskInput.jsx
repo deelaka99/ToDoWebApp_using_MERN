79bb02899301e563ace913c8860728689354e86c
deelaka99 <123wejith@gmail.com>
2023-12-29 15:26:26 +0530

    TaskInput component is ready for mobile devices

--- frontend/To_Do_Web_App/src/components/TaskInput.jsx
+++ frontend/To_Do_Web_App/src/components/TaskInput.jsx
@@ -1,14 +1,18 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 function TaskInput() {
   return (
     <div className="w-full h-auto flex items-center justify-center">
       <div className="flex pt-3 gap-1">
         <input
           type="text"
-          className="bg-white mobile:w-[170px] mobile:rounded-[3px] mobile:pl-2 mobile:text-[12px] text-textGrey outline-none focus:ring-2 drop-shadow placeholder:text-placeholderGrey"
+          className="bg-white font-inter mobile:w-[180px] mobile:rounded-[3px] mobile:pl-2 mobile:text-[12px] text-textGrey outline-none focus:ring-2 drop-shadow placeholder:text-placeholderGrey"
           placeholder="Enter your task here..."
         />
-        <button className="bg-green text-white h-auto mobile:w-[38px] mobile:px-1">
-          Add
+        <button className="bg-green text-white h-auto w-auto mobile:px-2 mobile:rounded-[4px] drop-shadow active:bg-darkGrey active:drop-shadow-md">
+          <FontAwesomeIcon className="mobile:text-[16px]" icon={faPlus} />
         </button>
       </div>
     </div>
