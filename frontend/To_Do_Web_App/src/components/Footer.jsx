76e787c0c386fba16e536b062b674706a0361b80
deelaka99 <123wejith@gmail.com>
2023-12-30 00:30:47 +0530

    Footer is ready for tablets

--- frontend/To_Do_Web_App/src/components/Footer.jsx
+++ frontend/To_Do_Web_App/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="font-inriaSans text-placeholderGrey font-light mobile:text-xs mobile:mt-10">
+    <div className="font-inriaSans text-placeholderGrey font-light mobile:text-xs mobile:mt-10 tablet:text-[16px] tablet:mt-16">
       ©PDW Kariyawasam
     </div>
   );
