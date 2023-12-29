75d21c1a596db649a4b664ad0c0f0f2f077b44b4
deelaka99 <123wejith@gmail.com>
2023-12-29 23:49:19 +0530

    added some change for TaskInput

--- frontend/To_Do_Web_App/src/components/TaskInput.jsx
+++ frontend/To_Do_Web_App/src/components/TaskInput.jsx
@@ -8,7 +8,7 @@ function TaskInput() {
       <div className="flex pt-3 gap-1">
         <input
           type="text"
-          className="bg-white font-inter mobile:w-[180px] mobile:rounded-[3px] mobile:pl-2 mobile:text-[12px] text-textGrey outline-none focus:ring-2 drop-shadow placeholder:text-placeholderGrey"
+          className="bg-white font-inter mobile:w-[185px] mobile:h-[35px] mobile:rounded-[3px] mobile:pl-2 mobile:text-[12px] text-textGrey outline-none focus:ring-2 drop-shadow placeholder:text-placeholderGrey"
           placeholder="Enter your task here..."
         />
         <button className="bg-green text-white h-auto w-auto mobile:px-2 mobile:rounded-[4px] drop-shadow active:bg-darkGrey active:drop-shadow-md">
