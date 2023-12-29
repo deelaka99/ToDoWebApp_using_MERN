3c486e4aa021a9eeb4cd8636d35cea9f1bb92d26
deelaka99 <123wejith@gmail.com>
2023-12-29 23:51:40 +0530

    added some changes to the title

--- frontend/To_Do_Web_App/src/components/Title.jsx
+++ frontend/To_Do_Web_App/src/components/Title.jsx
@@ -3,8 +3,8 @@ import React from "react"
 function Title() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-auto gap-1">
-      <p className="font-inter font-medium mobile:text-2xl text-darkGrey">Simple | To Do App</p>
-      <p className="text-thirdGrey font-jomhuria mobile:text-[10.5px]">A basic to do app created using MERN stack</p>
+      <p className="font-inter font-medium mobile:text-[28px] text-darkGrey">Simple | To Do App</p>
+      <p className="text-thirdGrey font-jomhuria mobile:text-[12px]">A basic to do app created using MERN stack</p>
     </div>
   )
 }
