ca31e1a512aa1565a00b9adc4aba4433d4f4253e
deelaka99 <123wejith@gmail.com>
2023-12-29 15:10:39 +0530

    partially done the taskInput component

--- frontend/To_Do_Web_App/src/components/TaskInput.jsx
+++ frontend/To_Do_Web_App/src/components/TaskInput.jsx
@@ -1,6 +1,17 @@
 function TaskInput() {
   return (
-    <div>TaskInput</div>
-  )
+    <div className="w-full h-auto flex items-center justify-center">
+      <div className="flex pt-3 gap-1">
+        <input
+          type="text"
+          className="bg-white mobile:w-[170px] mobile:rounded-[3px] mobile:pl-2 mobile:text-[12px] text-textGrey outline-none focus:ring-2 drop-shadow placeholder:text-placeholderGrey"
+          placeholder="Enter your task here..."
+        />
+        <button className="bg-green text-white h-auto mobile:w-[38px] mobile:px-1">
+          Add
+        </button>
+      </div>
+    </div>
+  );
 }
-export default TaskInput
+export default TaskInput;
