275c442a858807113ae36896f818ff783a4efa55
deelaka99 <123wejith@gmail.com>
2023-12-30 01:37:06 +0530

    Footer is ready for laptops

--- frontend/To_Do_Web_App/src/components/Footer.jsx
+++ frontend/To_Do_Web_App/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="font-inriaSans text-placeholderGrey font-light mobile:text-xs mobile:mt-10 tablet:text-[16px] tablet:mt-16">
+    <div className="font-inriaSans text-placeholderGrey font-light mobile:text-xs mobile:mt-10 tablet:text-[16px] tablet:mt-16 laptop:text-[12px] laptop:mt-10">
       ©PDW Kariyawasam
     </div>
   );
