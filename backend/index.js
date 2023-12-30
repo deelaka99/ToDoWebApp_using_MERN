800659834d899a365cf2c6cad3a9d1926f5ef00d
deelaka99 <123wejith@gmail.com>
2023-12-30 21:36:17 +0530

    tasks are saved to the database using backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const ToDoModel = require("./Models/Todo");
 
 const app = express();
 app.use(cors());
--- frontend/To_Do_Web_App/src/components/TaskInput.jsx
+++ frontend/To_Do_Web_App/src/components/TaskInput.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 function TaskInput() {
+  const [task, setTask] = useState();
+
+  const handleAdd = () => {
+    axios
+      .post("http://localhost:3001/add", { task: task })
+      .then((result) => location.reload())
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="w-full h-auto flex items-center justify-center">
       <div className="flex pt-3 mobile:gap-1 tablet:gap-2">
@@ -10,8 +20,15 @@ function TaskInput() {
           type="text"
           className="bg-white font-inter mobile:w-[227px] mobile:h-[36px] tablet:w-[400px] tablet:h-[55px] laptop:w-[350px] laptop:h-[45px] mobile:rounded-[3px] tablet:rounded-[5px] laptop:rounded-[4px] mobile:pl-2 tablet:pl-10 laptop:pl-7 mobile:text-[12px] tablet:text-[20px] laptop:text-[18px] text-textGrey outline-none focus:ring-2 drop-shadow placeholder:text-placeholderGrey"
           placeholder="Enter your task here..."
+          onChange={(event) => {
+            setTask(event.target.value);
+          }}
         />
-        <button className="bg-green text-white h-auto w-auto mobile:px-2 tablet:px-3 laptop:px-2 mobile:rounded-[6px] laptop:rounded-[5px] drop-shadow active:bg-darkGrey active:drop-shadow-md">
+        <button
+          onClick={handleAdd}
+          type="button"
+          className="bg-green text-white h-auto w-auto mobile:px-2 tablet:px-3 laptop:px-2 mobile:rounded-[6px] laptop:rounded-[5px] drop-shadow active:bg-darkGrey active:drop-shadow-md"
+        >
           <FontAwesomeIcon
             className="mobile:text-[16px] tablet:text-[48px] laptop:text-[40px]"
             icon={faPlus}
