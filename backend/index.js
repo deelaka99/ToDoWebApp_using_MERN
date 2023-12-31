48aac48f7ecdc4c3cd083732b4eebc69de18b13e
deelaka99 <123wejith@gmail.com>
2023-12-31 13:01:37 +0530

    added server details

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,14 @@ const cors = require("cors");
 const ToDoModel = require("./Models/Todo");
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: ["https://to-do-mren-app.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 mongoose.connect(
